977873f70ee4ce4cdfe26dd848250ae6ff0da2aa
Artem <kypidon71@gmail..com>
2022-12-01 23:43:22 +0300

    002

---
+++ 002/002.py
@@ -0,0 +1,7 @@
+def mult_lst(lst):
+    l = len(lst)//2 + 1 if len(lst) % 2 != 0 else len(lst)//2
+    new_lst = [lst[i]*lst[len(lst)-i-1] for i in range(l)]
+    print(new_lst)
+
+lst = list(map(int, input("Введите числа через пробел:\n").split()))
+mult_lst(lst)
